42381b0d0aa6592409a89273101f11dd8f19bb06
MaciejWarmbier <maciej.warmbier@gmail.com>
2022-07-09 21:30:13 +0200

    updated map

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float cameraSpeed = 20f;
+    [SerializeField] private float cameraMovementMargin = 20f;
     [SerializeField] private float minPositionX = 0;
     [SerializeField] private float maxPositionX = 80;
     [SerializeField] private float minPositionZ = -40;
@@ -15,21 +16,21 @@ public class CameraController : MonoBehaviour
     {
         Vector3 pos = transform.position;
 
-        if (Input.mousePosition.y >= Screen.height  - 10f && pos.z < maxPositionZ)
+        if (Input.mousePosition.y >= Screen.height  - cameraMovementMargin && pos.z < maxPositionZ)
         {
-            pos.z += cameraSpeed * Mathf.Min((1 - (Screen.height - Input.mousePosition.y) / 10f), 1.5f) * Time.deltaTime;
+            pos.z += cameraSpeed * Mathf.Min((1 - (Screen.height - Input.mousePosition.y) / cameraMovementMargin), 1.5f) * Time.deltaTime;
         }
-        else if (Input.mousePosition.y <= 10f && pos.z > minPositionZ)
+        else if (Input.mousePosition.y <= cameraMovementMargin && pos.z > minPositionZ)
         {
-            pos.z -= cameraSpeed * Mathf.Min((1 - Input.mousePosition.y / 10f), 1.5f) * Time.deltaTime;
+            pos.z -= cameraSpeed * Mathf.Min((1 - Input.mousePosition.y / cameraMovementMargin), 1.5f) * Time.deltaTime;
         }
-        if (Input.mousePosition.x >= Screen.width - 10f && pos.x < maxPositionX)
+        if (Input.mousePosition.x >= Screen.width - cameraMovementMargin && pos.x < maxPositionX)
         {
-            pos.x += cameraSpeed * Mathf.Min((1 - (Screen.width - Input.mousePosition.x) / 10f), 1.5f) * Time.deltaTime;
+            pos.x += cameraSpeed * Mathf.Min((1 - (Screen.width - Input.mousePosition.x) / cameraMovementMargin), 1.5f) * Time.deltaTime;
         }
-        else if (Input.mousePosition.x <= 10f && pos.x > minPositionX)
+        else if (Input.mousePosition.x <= cameraMovementMargin && pos.x > minPositionX)
         {
-            pos.x -= cameraSpeed * Mathf.Min((1 - Input.mousePosition.x / 10f), 1.5f) * Time.deltaTime;
+            pos.x -= cameraSpeed * Mathf.Min((1 - Input.mousePosition.x / cameraMovementMargin), 1.5f) * Time.deltaTime;
         }
 
         transform.position = pos;
